de3dc4768633284bbe0e30778756935e4791c38a
Simon Bonnedahl <simbo803@student.liu.se>
2022-12-09 21:55:10 +0100

    cleaning

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,7 @@ const Projects = () => {
                  <div className='project hidden'>
                     <h2>Title 2</h2>
                     <div className='spline-scene-phone'>
-                        <Spline scene="https://prod.spline.design/UxgG7s7IbukgFYv4/scene.splinecode" />
+                        <Spline scene="https://prod.spline.design/dcW1IC7Hj6K-d0Cj/scene.splinecode" />
                     </div>
                     <button className='button-5'>View</button>
                 </div>
